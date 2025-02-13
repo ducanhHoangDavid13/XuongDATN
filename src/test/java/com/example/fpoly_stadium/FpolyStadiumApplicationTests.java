89d1a08a1cb7ef6c56f94de786a5ec0b163c7c37
ducanhHoangDavid13 <ducdavidwork2024@gmail.com>
2025-02-13 13:53:17 +0700

    first commit

---
+++ src/test/java/com/example/fpoly_stadium/FpolyStadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fpoly_stadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpolyStadiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
