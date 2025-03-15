e9a21f00c892ad385c17bcdc66270ae513f99cd1
vuwoi <nqvu2004@gmail.com>
2025-03-15 16:12:28 +0700

    sua lai mot it

--- src/main/java/com/example/fpoly_stadium/entity/user/NhanVien.java
+++ src/main/java/com/example/fpoly_stadium/entity/user/NhanVien.java
@@ -13,26 +13,21 @@ import lombok.*;
 public class NhanVien extends CommonEntity {
 
     @Column(name = "ma_nhan_vien", unique = true, nullable = false)
-    @NotBlank(message = "Mã nhân viên không được để trống")
     private String maNhanVien;
 
     @Column(name = "ten_nhan_vien", nullable = false)
-    @NotBlank(message = "Tên nhân viên không được để trống")
     private String tenNhanVien;
 
     @Column(name = "mat_khau", nullable = false)
-    @NotBlank(message = "Mật khẩu không được để trống")
     private String matKhau;
 
     @Column(name = "email", unique = true)
-    @Email(message = "Email không hợp lệ")
     private String email;
 
     @Column(name = "gioi_tinh")
     private Boolean gioiTinh;
 
     @Column(name = "sdt")
-    @Pattern(regexp = "^(0|\\+84)[0-9]{9}$", message = "Số điện thoại không hợp lệ")
     private String sdt;
 
     @Column(name = "dia_chi")
@@ -40,9 +35,4 @@ public class NhanVien extends CommonEntity {
 
     @Column(name = "trang_thai")
     private Integer trangThai;
-
-    public static final class TrangThai {
-        public static final Integer HOAT_DONG = 1;
-        public static final Integer KHONG_HOAT_DONG = 0;
-    }
 }
